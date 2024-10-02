bd4cddd5cc6bd2f6763e8987c38fb201830fb1ac
Minh Tuấn <131225214+tuannguyentikijob@users.noreply.github.com>
2024-10-02 15:00:20 +0700

    Add project files.

---
+++ Controllers/Auth/ForgotPasswordController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GallifreyPlanet.Controllers.Auth
+{
+	public class ForgotPasswordController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Controllers/Auth/ResetPasswordController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GallifreyPlanet.Controllers.Auth
+{
+	public class ResetPasswordController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
