6401bbcf914811761e04d390ca00b93f6e5d1a18
Minh Tuấn Nguyễn <84488169+Minhtuan622@users.noreply.github.com>
2024-10-02 23:32:01 +0700

    feat: improve form login and signup

--- Controllers/Auth/LoginController.cs
+++ Controllers/Auth/LoginController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GallifreyPlanet.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace GallifreyPlanet.Controllers.Auth
 {
     public class LoginController : Controller
     {
+        // GET: /Login
         public IActionResult Index()
         {
             return View();
+        }
+
+        // POST: /Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Check user credentials against database
+                // For now, we'll just simulate a successful login
+                
+                // If login is successful, redirect to home page
+                return RedirectToAction("Index", "Home");
+            }
+
+            // If login fails, show error message
+            ModelState.AddModelError("", "Invalid login attempt.");
+            return View(user);
+        }
+
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }
--- Controllers/Auth/SignupController.cs
+++ Controllers/Auth/SignupController.cs
@@ -1,12 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GallifreyPlanet.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace GallifreyPlanet.Controllers.Auth
 {
     public class SignupController : Controller
     {
+        // GET: /Signup
         public IActionResult Index()
         {
             return View();
+        }
+
+        // POST: /Signup
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Check if email already exists in the database
+
+                // Hash the password
+                user.Password = HashPassword(user.Password);
+
+                // TODO: Save user to database
+
+                // Redirect to login page
+                return RedirectToAction("Index", "Login");
+            }
+
+            return View(user);
+        }
+
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }
