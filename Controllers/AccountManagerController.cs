2ed89cda11a9cf55cfe54e50a0a18a1da097ceb7
Minh Tuấn <131225214+tuannguyentikijob@users.noreply.github.com>
2024-10-09 14:30:00 +0700

    feat: add account manager

---
+++ Controllers/AccountManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GallifreyPlanet.Controllers
+{
+	public class AccountManagerController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
