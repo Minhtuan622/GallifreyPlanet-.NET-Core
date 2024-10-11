562b353604365fb72fe786d45ad5e0d2821d2a68
Minh Tuấn Nguyễn <84488169+Minhtuan622@users.noreply.github.com>
2024-10-11 22:45:28 +0700

    feat: improve UX UI for account

--- ViewModels/PublicProfileViewModel.cs
+++ ViewModels/PublicProfileViewModel.cs
@@ -6,5 +6,29 @@ namespace GallifreyPlanet.ViewModels
         public string? Name { get; set; }
         public string? Avatar { get; set; }
         public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? Website { get; set; }
+        public string? Github { get; set; }
+        public string? Twitter { get; set; }
+        public string? Instagram { get; set; }
+        public string? Facebook { get; set; }
+        public List<RecentPost> RecentPosts { get; set; } = new List<RecentPost>();
+        public RecentActivities RecentActivities { get; set; } = new RecentActivities();
+    }
+
+    public class RecentPost
+    {
+        public string Title { get; set; } = string.Empty;
+        public int ViewPercentage { get; set; }
+    }
+
+    public class RecentActivities
+    {
+        public int CommentPercentage { get; set; }
+        public int LikePercentage { get; set; }
+        public int SharePercentage { get; set; }
+        public int RatingPercentage { get; set; }
+        public int FollowPercentage { get; set; }
     }
 }
