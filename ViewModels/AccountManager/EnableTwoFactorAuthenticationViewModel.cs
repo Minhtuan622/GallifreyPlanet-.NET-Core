710f193adc1e56a4ec94e5266b53b6642614c247
Minh Tuấn <131225214+tuannguyentikijob@users.noreply.github.com>
2024-10-16 14:54:36 +0700

    feat: add qr code js library

--- ViewModels/AccountManager/EnableTwoFactorAuthenticationViewModel.cs
+++ ViewModels/AccountManager/EnableTwoFactorAuthenticationViewModel.cs
@@ -4,11 +4,11 @@ namespace GallifreyPlanet.ViewModels.AccountManager
 {
     public class EnableTwoFactorAuthenticationViewModel
     {
-        public string Token { get; set; }
+        public string? Token { get; set; }
 
         [Required(ErrorMessage = "Vui lòng nhập mã xác thực.")]
-        [StringLength(7, MinimumLength = 6, ErrorMessage = "Mã xác thực phải có 6 ký tự.")]
+        [StringLength(maximumLength: 7, MinimumLength = 6, ErrorMessage = "Mã xác thực phải có 6 ký tự.")]
         [Display(Name = "Mã xác thực")]
-        public string VerificationCode { get; set; }
+        public string? VerificationCode { get; set; }
     }
 }
