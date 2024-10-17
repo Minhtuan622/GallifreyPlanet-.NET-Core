3d60b1e3c1eeaa32e193fac79e5c40209c6dd0dd
Minh Tuấn Nguyễn <84488169+Minhtuan622@users.noreply.github.com>
2024-10-17 22:10:28 +0700

    feat: add friend request model

---
+++ Models/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GallifreyPlanet.Models
+{
+    [Table(name: "friends")]
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public string? FriendId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
---
+++ Models/FriendRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GallifreyPlanet.Models
+{
+    [Table(name: "FriendRequests")]
+    public class FriendRequest
+    {
+        public int Id { get; set; }
+        public string? RequesterId { get; set; }
+        public string? ReceiverId { get; set; }
+        public int? Status { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
